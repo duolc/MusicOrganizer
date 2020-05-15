550eb38cadb924985e9c015c4db97ce1572274e9
Ryan <ryan.johnson.j@gmail.com>
2020-05-15 11:10:31 -0400

    Update
    Allows input off separator

--- organizeMusic.py
+++ organizeMusic.py
@@ -6,13 +6,14 @@ print ("Current Directory is:", os.getcwd())
 cont = input("Organize Files in current directory (Y)es, (N)o\n")
 path =  os.getcwd()
 if cont in ["Y", "y", "Yes", "yes"]:
+    separator = input("Input separator for Artist/Album/Song \nOnly supports a single type of separator\n")
     files = os.listdir(path)
     for origName in files:
         if origName.endswith('.flac'):
-            artistName = origName.split("_-_",3)[0]
-            albumName = origName.split("_-_",3)[1]
-            trackNumber = origName.split("_-_",3)[2]
-            songName = origName.split("_-_",3)[3]
+            artistName = origName.split(separator,3)[0]
+            albumName = origName.split(separator,3)[1]
+            trackNumber = origName.split(separator,3)[2]
+            songName = origName.split(separator,3)[3]
             if not os.path.exists(artistName):
                 os.makedirs(artistName)
             if not os.path.exists(artistName +"\\"+albumName):
